587e9cc9d82023dcd6682d5bc9f301ff4e083a59
KK <karanjakelvin77@gmail.com>
2022-03-15 22:17:13 +0300

    Heroku Deployment

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS=True
     UPLOADED_PHOTOS_DEST='app/static/photos'
     MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT=465 
